a1615bd8e3aeb61fee8745847a22a01c7d3fb6a3
Donald Steinert <steinert.donald@gmail.com>
2015-04-28 19:33:27 -0400

    completed chapter 7

---
+++ ch7/numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    unsigned int x = 255;
+    printf("x is %d.\n", x);
+    printf("In octal, x is %o.\n", x);
+    printf("In hexadecimal, x is %x.\n", x);
+    
+    return 0;
+}
